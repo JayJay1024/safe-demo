f9718fa1391adad7086b21fa9be726c37a09a553
JayJay1024 <lygm1062@gmail.com>
2024-03-17 23:31:00 +0800

    Create RainbowProvider

--- next.config.mjs
+++ next.config.mjs
@@ -1,4 +1,12 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {};
+const nextConfig = {
+  output: "export",
+  images: { unoptimized: true },
+  reactStrictMode: true,
+  webpack: (config) => {
+    config.externals.push("pino-pretty", "lokijs", "encoding");
+    return config;
+  },
+};
 
 export default nextConfig;
---
+++ src/providers/rainbow-provider.tsx
@@ -0,0 +1,50 @@
+"use client";
+
+import "@rainbow-me/rainbowkit/styles.css";
+import { getDefaultConfig, RainbowKitProvider } from "@rainbow-me/rainbowkit";
+import { WagmiProvider } from "wagmi";
+import { Chain, sepolia } from "wagmi/chains";
+import { QueryClientProvider, QueryClient } from "@tanstack/react-query";
+import { PropsWithChildren } from "react";
+
+const darwinia: Chain = {
+  id: 46,
+  name: "Darwinia",
+  nativeCurrency: { name: "RING", symbol: "RING", decimals: 18 },
+  rpcUrls: {
+    default: {
+      http: ["https://rpc.darwinia.network"],
+    },
+  },
+  blockExplorers: {
+    default: {
+      name: "Subscan",
+      url: "https://darwinia.subscan.io",
+    },
+  },
+  contracts: {
+    multicall3: {
+      address: "0xcA11bde05977b3631167028862bE2a173976CA11",
+      blockCreated: 251739,
+    },
+  },
+};
+
+const appName = "Safe Demo";
+const projectId = process.env.NEXT_PUBLIC_WALLET_CONNECT_ID || "";
+const config = getDefaultConfig({
+  appName,
+  projectId,
+  chains: [sepolia, darwinia],
+});
+const queryClient = new QueryClient();
+
+export default function RainbowProvider({ children }: PropsWithChildren<unknown>) {
+  return (
+    <WagmiProvider config={config}>
+      <QueryClientProvider client={queryClient}>
+        <RainbowKitProvider>{children}</RainbowKitProvider>
+      </QueryClientProvider>
+    </WagmiProvider>
+  );
+}
